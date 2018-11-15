b748ac59895ae5e0fe9f4f8b344fd27e0b241ae2
Julien Bras <julienbras@sidoine.org>
2018-11-15 11:50:14 -0500

    change mongo env variable name for prod

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 // Set up mongoose connection
 const mongoose = require('mongoose');
 let dev_db_url = 'mongodb://localhost/test';
-let mongoDB = process.env.MONGODB_URI || dev_db_url;
+let mongoDB = process.env.MONGO_URL || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true});
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
