6859c495edb0cd24be4ba4636ef5dfc421c9a3c1
Michele Barbiero <michele.barbiero@wiiisdom.com>
2022-02-14 13:55:37 -0500

    Move from Craco to CRA as tailwind doesnt require it anymore. Update packages where possible.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,19 @@
-// add your custom configurations
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/*.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,html}",
+  ],
   theme: {
     extend: {
-      colors: {
-        primary: '#61DAFB',
-        secondary: '#282C34'
+      extend: {
+        colors: {
+          primary: '#61DAFB',
+          secondary: '#282C34'
+        },
+        animation: {
+          'spin-slow': 'spin 20s linear infinite'
+        }
       },
-      animation: {
-        'spin-slow': 'spin 20s linear infinite'
-      }
-    }
+    },
   },
-  variants: {
-    extend: {}
-  },
-  plugins: []
-};
+  plugins: [],
+}
