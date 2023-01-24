2d8d8a0dd9ab4fee5230a7ca304dc7f4313ca64e
kirti.patel <kirti.patel@wiiisdom.com>
2023-01-24 16:50:25 -0500

    remove sonar errors

--- frontend/src/components/__tests__/LaunchTemplateContainer.test.js
+++ frontend/src/components/__tests__/LaunchTemplateContainer.test.js
@@ -8,7 +8,8 @@ jest.mock('react-query', () => ({
 
 describe('LaunchTemplateContainer', () => {
   it('should render with sorted data', () => {
-    const data = [{id: 2, name: 'John', version: 1}, {id: 1, name: 'Mary', version: 3}, {id: 3, name: 'Mike', version: 2}];
+    const data = [{id: 1, name: 'Mary', version: 3}, {id: 2, name: 'John', version: 1}, {id: 3, name: 'Mike', version: 2}];
+    const sortedData = data.sort((a, b) => a.name.localeCompare(b.name))
     const search = '';
     const selectedLaunchTemplateId = 3;
     const setLaunchTemplate = jest.fn();
@@ -21,7 +22,6 @@ describe('LaunchTemplateContainer', () => {
   );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
-    const sortedData = data.sort((a, b) => a.name.localeCompare(b.name))
     expect(data).toEqual(expect.arrayContaining(sortedData));
   });
 });
