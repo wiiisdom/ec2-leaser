689a9d236cadd09032665e24a5e1d67486f35c92
Julien Bras <julienbras@sidoine.org>
2023-03-08 17:06:19 -0500

    small upgrade ;)

---
+++ sst.config.ts
@@ -0,0 +1,24 @@
+import { SSTConfig } from "sst";
+import { API } from "./stacks/API";
+
+export default {
+  config(_input) {
+    return {
+      name: "ec2-leaser",
+      region: "us-east-1"
+    };
+  },
+  stacks(app) {
+    app.setDefaultFunctionProps({
+      logRetention: "three_months"
+    });
+    app.stack(API, {
+      id: "backend-stack",
+      tags: {
+        costcenter: "eng:lab",
+        project: "ec2-leaser",
+        owner: "lab@wiiisdom.com"
+      }
+    });
+  }
+} satisfies SSTConfig;
