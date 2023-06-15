8737bda0dca69adfcfc8b58332ac8c37db53eddb
Infra Wiiisdom <infra@wiiisdom.com>
2023-06-15 19:31:10 +0000

    Merged in renovate/vitest-monorepo (pull request #74)
    Update vitest monorepo to ^0.32.0
    
    * Update vitest monorepo to ^0.32.0
    
    * fix coverage
    
    
    Approved-by: Julien BRAS

--- packages/functions/vitest.config.ts
+++ packages/functions/vitest.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     threads: false,
     outputFile: "sonar-report.xml",
     coverage: {
-      provider: "istanbul",
       reporter: ["lcov", "text"]
     }
   }
