3c3b101d49a77d78c2627c165d935a15b10cfbcb
Julien BRAS <julien.bras@wiiisdom.com>
2024-05-14 15:33:17 +0000

    Merged in feature/LAB-1230/revert-ipv4-public (pull request #554)
    LAB-1230: Revert to public IPv4
    
    * feat: revert to public ipv4
    
    refs: LAB-1230
    
    
    Approved-by: Adrian MITRICA
    Approved-by: François LAPIERRE

--- packages/functions/src/handlers/api/start-instance.ts
+++ packages/functions/src/handlers/api/start-instance.ts
@@ -1,10 +1,4 @@
-import {
-  DescribeLaunchTemplateVersionsCommand,
-  EC2Client,
-  LaunchTemplateTagSpecification,
-  RunInstancesCommand,
-  RunInstancesCommandInput,
-} from '@aws-sdk/client-ec2';
+import { EC2Client, RunInstancesCommand, RunInstancesCommandInput } from '@aws-sdk/client-ec2';
 import { SecureHandler } from 'src/utils/handlerUtils';
 
 const client = new EC2Client({});
@@ -22,25 +16,6 @@ export const handler = SecureHandler(async event => {
     const name = request.title;
     const { owner, costCenter, schedule } = request;
 
-    // retrieve launchTemplate to extract imageId, instanceType, keyName and securityGroupIds
-    const launchTemplate = await client.send(
-      new DescribeLaunchTemplateVersionsCommand({
-        LaunchTemplateId: launchTemplateId,
-        Versions: ['$Latest'],
-      })
-    );
-    const launchTemplateData = launchTemplate.LaunchTemplateVersions?.[0].LaunchTemplateData;
-
-    const instanceTags =
-      launchTemplateData?.TagSpecifications?.find(
-        (tag: LaunchTemplateTagSpecification) => tag.ResourceType === 'instance'
-      )?.Tags || [];
-
-    const volumeTags =
-      launchTemplateData?.TagSpecifications?.find(
-        (tag: LaunchTemplateTagSpecification) => tag.ResourceType === 'volume'
-      )?.Tags || [];
-
     const tags = [
       {
         Key: 'Name',
@@ -55,6 +30,10 @@ export const handler = SecureHandler(async event => {
         Value: costCenter,
       },
       {
+        Key: 'project',
+        Value: 'ec2-leaser',
+      },
+      {
         Key: 'owner',
         Value: owner,
       },
@@ -67,30 +46,27 @@ export const handler = SecureHandler(async event => {
     const params: RunInstancesCommandInput = {
       MaxCount: 1,
       MinCount: 1,
-      ImageId: launchTemplateData?.ImageId,
-      InstanceType: launchTemplateData?.InstanceType,
-      KeyName: launchTemplateData?.KeyName,
+      LaunchTemplate: {
+        LaunchTemplateId: launchTemplateId,
+      },
       TagSpecifications: [
         {
           ResourceType: 'instance',
-          Tags: [...tags, ...instanceTags],
+          Tags: tags,
         },
         {
           ResourceType: 'volume',
-          Tags: [...tags, ...volumeTags],
+          Tags: tags,
+        },
+        {
+          ResourceType: 'network-interface',
+          Tags: tags,
         },
       ],
       MetadataOptions: {
         HttpEndpoint: 'enabled',
         HttpTokens: 'required',
       },
-      NetworkInterfaces: [
-        {
-          DeviceIndex: 0,
-          AssociatePublicIpAddress: false,
-          Groups: launchTemplateData?.SecurityGroupIds,
-        },
-      ],
     };
 
     const data = await client.send(new RunInstancesCommand(params));
--- packages/functions/test/handlers/api/start-instance.test.ts
+++ packages/functions/test/handlers/api/start-instance.test.ts
@@ -1,253 +1,117 @@
 import { handler } from 'src/handlers/api/start-instance';
-import { it, expect, describe, vi } from 'vitest';
+import { it, expect, vi } from 'vitest';
 import { mockClient } from 'aws-sdk-client-mock';
 import { mock } from 'vitest-mock-extended';
 
 import { APIGatewayProxyEventV2, Context } from 'aws-lambda';
-import {
-  DescribeLaunchTemplateVersionsCommand,
-  EC2Client,
-  RunInstancesCommand,
-} from '@aws-sdk/client-ec2';
+import { EC2Client, RunInstancesCommand } from '@aws-sdk/client-ec2';
 
 vi.mock('src/utils/authUtils');
 
-describe('start instance', () => {
-  it('handler must start instance with tags', async () => {
-    const ec2ClientMock = mockClient(EC2Client);
-    ec2ClientMock.on(DescribeLaunchTemplateVersionsCommand).resolves({
-      LaunchTemplateVersions: [
-        {
-          LaunchTemplateData: {
-            ImageId: 'ami-id',
-            InstanceType: 't2.micro',
-            KeyName: 'key-name',
-            SecurityGroupIds: ['sg-id'],
-            TagSpecifications: [
-              {
-                ResourceType: 'instance',
-                Tags: [
-                  {
-                    Key: 'pool',
-                    Value: 'DEV',
-                  },
-                ],
-              },
-              {
-                ResourceType: 'volume',
-                Tags: [
-                  {
-                    Key: 'volume',
-                    Value: 'volume',
-                  },
-                ],
-              },
-            ],
-          },
-          VersionNumber: 1,
-        },
-      ],
-    });
+it('handler must start instance', async () => {
+  const ec2ClientMock = mockClient(EC2Client);
 
-    ec2ClientMock.on(RunInstancesCommand).resolves({
-      Instances: [
-        {
-          InstanceId: 'id',
-          PrivateIpAddress: '1.1.1.1',
-        },
-      ],
-    });
-    const event = mock<APIGatewayProxyEventV2>();
-    event.body = JSON.stringify({
-      instanceId: 'id',
-      owner: 'owner',
-      costCenter: 'costCenter',
-      schedule: 'schedule',
-      title: 'title',
-    });
-
-    const context = mock<Context>();
-    const result = await handler(event, context);
-
-    const runInstanceCommandInput = ec2ClientMock.call(1).args[0].input;
-    expect(runInstanceCommandInput).toHaveProperty('ImageId', 'ami-id');
-    expect(runInstanceCommandInput).toHaveProperty('InstanceType', 't2.micro');
-    expect(runInstanceCommandInput).toHaveProperty('TagSpecifications', [
+  ec2ClientMock.on(RunInstancesCommand).resolves({
+    Instances: [
       {
-        ResourceType: 'instance',
-        Tags: [
-          { Key: 'Name', Value: 'title' },
-          { Key: 'Ec2LeaserDuration', Value: '6' },
-          { Key: 'costcenter', Value: 'costCenter' },
-          { Key: 'owner', Value: 'owner' },
-          { Key: 'schedule', Value: 'schedule' },
-          { Key: 'pool', Value: 'DEV' },
-        ],
+        InstanceId: 'id',
+        PrivateIpAddress: '1.1.1.1',
       },
-      {
-        ResourceType: 'volume',
-        Tags: [
-          { Key: 'Name', Value: 'title' },
-          { Key: 'Ec2LeaserDuration', Value: '6' },
-          { Key: 'costcenter', Value: 'costCenter' },
-          { Key: 'owner', Value: 'owner' },
-          { Key: 'schedule', Value: 'schedule' },
-          { Key: 'volume', Value: 'volume' },
-        ],
-      },
-    ]);
-
-    expect(result.statusCode).toStrictEqual(200);
-    expect(result.body).toStrictEqual(
-      JSON.stringify({
-        instanceId: 'id',
-        privateIp: '1.1.1.1',
-      })
-    );
+    ],
+  });
+  const event = mock<APIGatewayProxyEventV2>();
+  event.body = JSON.stringify({
+    instanceId: 'id',
+    owner: 'owner',
+    costCenter: 'costCenter',
+    schedule: 'schedule',
+    title: 'title',
   });
 
-  it('handler must start instance without tags', async () => {
-    const ec2ClientMock = mockClient(EC2Client);
-    ec2ClientMock.on(DescribeLaunchTemplateVersionsCommand).resolves({
-      LaunchTemplateVersions: [
-        {
-          LaunchTemplateData: {
-            ImageId: 'ami-id',
-            InstanceType: 't2.micro',
-            KeyName: 'key-name',
-            SecurityGroupIds: ['sg-id'],
-          },
-          VersionNumber: 1,
-        },
+  const context = mock<Context>();
+  const result = await handler(event, context);
+
+  const runInstanceCommandInput = ec2ClientMock.call(0).args[0].input;
+  expect(runInstanceCommandInput).toHaveProperty('LaunchTemplate', {
+    LaunchTemplateId: 'id',
+  });
+  expect(runInstanceCommandInput).toHaveProperty('TagSpecifications', [
+    {
+      ResourceType: 'instance',
+      Tags: [
+        { Key: 'Name', Value: 'title' },
+        { Key: 'Ec2LeaserDuration', Value: '6' },
+        { Key: 'costcenter', Value: 'costCenter' },
+        { Key: 'project', Value: 'ec2-leaser' },
+        { Key: 'owner', Value: 'owner' },
+        { Key: 'schedule', Value: 'schedule' },
       ],
-    });
-
-    ec2ClientMock.on(RunInstancesCommand).resolves({
-      Instances: [
-        {
-          InstanceId: 'id',
-          PrivateIpAddress: '1.1.1.1',
-        },
+    },
+    {
+      ResourceType: 'volume',
+      Tags: [
+        { Key: 'Name', Value: 'title' },
+        { Key: 'Ec2LeaserDuration', Value: '6' },
+        { Key: 'costcenter', Value: 'costCenter' },
+        { Key: 'project', Value: 'ec2-leaser' },
+        { Key: 'owner', Value: 'owner' },
+        { Key: 'schedule', Value: 'schedule' },
       ],
-    });
-    const event = mock<APIGatewayProxyEventV2>();
-    event.body = JSON.stringify({
-      instanceId: 'id',
-      owner: 'owner',
-      costCenter: 'costCenter',
-      schedule: 'schedule',
-      title: 'title',
-    });
-
-    const context = mock<Context>();
-    const result = await handler(event, context);
-
-    const runInstanceCommandInput = ec2ClientMock.call(1).args[0].input;
-    expect(runInstanceCommandInput).toHaveProperty('ImageId', 'ami-id');
-    expect(runInstanceCommandInput).toHaveProperty('InstanceType', 't2.micro');
-    expect(runInstanceCommandInput).toHaveProperty('TagSpecifications', [
-      {
-        ResourceType: 'instance',
-        Tags: [
-          { Key: 'Name', Value: 'title' },
-          { Key: 'Ec2LeaserDuration', Value: '6' },
-          { Key: 'costcenter', Value: 'costCenter' },
-          { Key: 'owner', Value: 'owner' },
-          { Key: 'schedule', Value: 'schedule' },
-        ],
-      },
-      {
-        ResourceType: 'volume',
-        Tags: [
-          { Key: 'Name', Value: 'title' },
-          { Key: 'Ec2LeaserDuration', Value: '6' },
-          { Key: 'costcenter', Value: 'costCenter' },
-          { Key: 'owner', Value: 'owner' },
-          { Key: 'schedule', Value: 'schedule' },
-        ],
-      },
-    ]);
-
-    expect(result.statusCode).toStrictEqual(200);
-    expect(result.body).toStrictEqual(
-      JSON.stringify({
-        instanceId: 'id',
-        privateIp: '1.1.1.1',
-      })
-    );
-  });
-
-  it('handler must return a error if no body', async () => {
-    const event = mock<APIGatewayProxyEventV2>();
-    event.body = undefined;
-    const context = mock<Context>();
-    const result = await handler(event, context);
-    expect(result.statusCode).toStrictEqual(500);
-    expect(result.body).toStrictEqual('Missing parameters');
-  });
-  it('handler must return a error if aws error on describe launch template', async () => {
-    const ec2ClientMock = mockClient(EC2Client);
-
-    ec2ClientMock.on(DescribeLaunchTemplateVersionsCommand).rejects('AWS error');
-    const event = mock<APIGatewayProxyEventV2>();
-    event.body = JSON.stringify({
-      instanceId: 'id',
-    });
-    const context = mock<Context>();
-    const result = await handler(event, context);
-    expect(result.statusCode).toStrictEqual(500);
-    expect(result.body).toStrictEqual('AWS error');
-  });
-  it('handler must return a error if aws error on run instance', async () => {
-    const ec2ClientMock = mockClient(EC2Client);
-    ec2ClientMock.on(DescribeLaunchTemplateVersionsCommand).resolves({
-      LaunchTemplateVersions: [
-        {
-          LaunchTemplateData: {
-            ImageId: 'ami-id',
-            InstanceType: 't2.micro',
-            KeyName: 'key-name',
-            SecurityGroupIds: ['sg-id'],
-          },
-          VersionNumber: 1,
-        },
+    },
+    {
+      ResourceType: 'network-interface',
+      Tags: [
+        { Key: 'Name', Value: 'title' },
+        { Key: 'Ec2LeaserDuration', Value: '6' },
+        { Key: 'costcenter', Value: 'costCenter' },
+        { Key: 'project', Value: 'ec2-leaser' },
+        { Key: 'owner', Value: 'owner' },
+        { Key: 'schedule', Value: 'schedule' },
       ],
-    });
-    ec2ClientMock.on(RunInstancesCommand).rejects('AWS error');
-    const event = mock<APIGatewayProxyEventV2>();
-    event.body = JSON.stringify({
+    },
+  ]);
+
+  expect(result.statusCode).toStrictEqual(200);
+  expect(result.body).toStrictEqual(
+    JSON.stringify({
       instanceId: 'id',
-    });
-    const context = mock<Context>();
-    const result = await handler(event, context);
-    expect(result.statusCode).toStrictEqual(500);
-    expect(result.body).toStrictEqual('AWS error');
+      privateIp: '1.1.1.1',
+    })
+  );
+});
+
+it('handler must return a error if no body', async () => {
+  const event = mock<APIGatewayProxyEventV2>();
+  event.body = undefined;
+  const context = mock<Context>();
+  const result = await handler(event, context);
+  expect(result.statusCode).toStrictEqual(500);
+  expect(result.body).toStrictEqual('Missing parameters');
+});
+
+it('handler must return a error if aws error on run instance', async () => {
+  const ec2ClientMock = mockClient(EC2Client);
+  ec2ClientMock.on(RunInstancesCommand).rejects('AWS error');
+  const event = mock<APIGatewayProxyEventV2>();
+  event.body = JSON.stringify({
+    instanceId: 'id',
   });
-  it('handler must return a error if no instance in result', async () => {
-    const ec2ClientMock = mockClient(EC2Client);
-    ec2ClientMock.on(DescribeLaunchTemplateVersionsCommand).resolves({
-      LaunchTemplateVersions: [
-        {
-          LaunchTemplateData: {
-            ImageId: 'ami-id',
-            InstanceType: 't2.micro',
-            KeyName: 'key-name',
-            SecurityGroupIds: ['sg-id'],
-          },
-          VersionNumber: 1,
-        },
-      ],
-    });
-    ec2ClientMock.on(RunInstancesCommand).resolves({
-      Instances: [],
-    });
-    const event = mock<APIGatewayProxyEventV2>();
-    event.body = JSON.stringify({
-      instanceId: 'id',
-    });
-    const context = mock<Context>();
-    const result = await handler(event, context);
-    expect(result.statusCode).toStrictEqual(500);
-    expect(result.body).toStrictEqual('Wrong result from the EC2 API');
+  const context = mock<Context>();
+  const result = await handler(event, context);
+  expect(result.statusCode).toStrictEqual(500);
+  expect(result.body).toStrictEqual('AWS error');
+});
+it('handler must return a error if no instance in result', async () => {
+  const ec2ClientMock = mockClient(EC2Client);
+  ec2ClientMock.on(RunInstancesCommand).resolves({
+    Instances: [],
   });
+  const event = mock<APIGatewayProxyEventV2>();
+  event.body = JSON.stringify({
+    instanceId: 'id',
+  });
+  const context = mock<Context>();
+  const result = await handler(event, context);
+  expect(result.statusCode).toStrictEqual(500);
+  expect(result.body).toStrictEqual('Wrong result from the EC2 API');
 });
